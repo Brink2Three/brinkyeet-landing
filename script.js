6aabc7fd0461cbc54b8f51c5c628fac50d0b23d9
Brink2Three <drewisbrink@gmail.com>
2025-01-23 17:14:41 -0500

    updated MOTD and typing

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function type() {
   if (index < text.length) {
     typedText.textContent += text[index];
     index++;
-    setTimeout(type, 100); // Typing speed in milliseconds
+    setTimeout(type, 20); // Typing speed in milliseconds
   } else {
     flashCursor(); // Start the cursor flashing after typing finishes
   }
@@ -24,6 +24,33 @@ function fadeOut() {
   setTimeout(() => {
     window.location.href = "index.html"; // Redirect to your main website
   }, 20000000); // Adjust timing to match fade-out duration
+}
+
+function ipdisplay() {
+        document.addEventListener("DOMContentLoaded", function() {
+            // Fetch the IP address from the API
+            fetch("https://api.ipify.org?format=json")
+                .then(response => response.json())
+                .then(data => {
+                    // Display the IP address on the screen
+                    document.getElementById("ip-address").textContent = data.ip;
+                })
+                .catch(error => {
+                    console.error("Error fetching IP address:", error);
+                });
+        });
+}
+
+function storelastLogin() {
+  let lastLoginTimestamp = localStorage.getItem('last-login-timestamp');
+  if (!lastLoginTimestamp) {
+    lastLoginTimestamp = new Date().getTime();
+  }
+  
+  document.getElementById('typing-text').addEventListener('input', () => {
+    const currentTime = new Date().getTime();
+    localStorage.setItem('last-login-timestamp', currentTime);
+  });
 }
 
 type();
